04b0f4a0aaec65bfe89556b6fbb23bd62d28e4b4
Lrelni <lrelni.official@gmail.com>
2025-09-07 07:08:55 -0600

    add 'modified by lrelni' line

--- Lrelni/keymap.c
+++ Lrelni/keymap.c
@@ -1,4 +1,5 @@
 /* Copyright 2023 @ Keychron (https://www.keychron.com)
+ * Modified 2025 by Lrelni
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
